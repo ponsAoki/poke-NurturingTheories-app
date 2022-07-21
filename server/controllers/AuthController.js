a9c4857b0e012bcf59e95733cc2cf9b40ecbb3a8
ponsAoki <aokishogo20pk@gmail.com>
2022-07-21 23:15:35 +0900

    サーバーサイドで、ユーザー登録・ログイン機能完了

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -30,6 +30,43 @@ const register = (req, res, next) => {
 
 }
 
+const login = (req, res, next) => {
+    const userName = req.body.username
+    const password = req.body.password
+
+    //userNameが一致するレコードをDBから取得
+    User.findOne({ email: userName })
+        .then(user => {
+            if (user) {
+                //リクエスト(POST)されたpasswrodをDBのものと比較
+                bcrypt.compare(password, user.password, (err, result) => {
+                    if (err) {
+                        res.json({
+                            error: err
+                        })
+                    }
+                    //passwordが一致した場合トークンを作成(有効期間1日)
+                    if (result) {
+                        let token = jwt.sign({ name: user.name }, 'verySecretValue', { expiresIn: '1day' })
+                        res.json({
+                            message: 'ログイン完了!',
+                            token
+                        })
+                    } else {
+                        res.json({
+                            message: 'パスワードが違います'
+                        })
+                    }
+                })
+            } else {
+                res.json({
+                    message: 'ユーザーが見当たりません'
+                })
+            }
+        })
+}
+
 module.exports = {
-    register
+    register,
+    login
 }
