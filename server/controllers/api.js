4cd884a870d71e6d49527e60d868bf53084d4471
ponsAoki <aokishogo20pk@gmail.com>
2022-08-02 22:50:53 +0900

    投稿削除のAPI少し変更

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -49,18 +49,18 @@ module.exports = class API {
                 res.status(404).json({ message: error.message })
             }
         }
-        //育成論破棄
+        //育成論削除
     static async deletePost(req, res) {
         const id = req.params.id
         try {
             const result = await Post.findByIdAndDelete(id)
-            if (result.image != '') {
-                try {
-                    fs.unlinkSync('./uploads/' + result.image)
-                } catch (error) {
-                    console.log(error);
-                }
-            }
+                // if (result.image != '') {
+                //     try {
+                //         fs.unlinkSync('./uploads/' + result.image)
+                //     } catch (error) {
+                //         console.log(error);
+                //     }
+                // }
             res.status(200).json({ message: '育成論を消去しました。' })
         } catch (error) {
             res.status(404).json({ message: error.message })
